e933056edfd0138f9dc808184e35307e747cbf2c
todwal g <66479707+g8todwal@users.noreply.github.com>
2020-07-30 22:33:31 +0530

    Add files via upload

---
+++ precache-manifest.b85adb5dae9356d6d65db85bc32974e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d94b7ea5b9f2894e21ac80bc58ed1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dcee0628d3a64fc432c",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0423cbbe326e62779a3c",
+    "url": "/static/js/2.8c69ddbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c69ddbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dcee0628d3a64fc432c",
+    "url": "/static/js/main.db738dd8.chunk.js"
+  },
+  {
+    "revision": "a193d4d1c2eea9c6a65e",
+    "url": "/static/js/runtime-main.8c17383c.js"
+  }
+]);
